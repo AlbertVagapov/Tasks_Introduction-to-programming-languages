002e2e414605dd56b5131bc0a7853692a14a7e53
Albert Vagapov <va3@list.ru>
2022-02-01 12:02:45 +0500

    Add task1

---
+++ task001_^2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Вывести квадрат числа
+
+Console.Write("Введите число")
+
+
+
+
+
+
+//Console.WriteLine("Введите число N");
+
+//if ( m == n*n )
+//{
+   // Console.WriteLine(n);
+//}
+//else 
+//{
+//    Console.WriteLine(m);
+//}
+
+
+
+//Console.Write("Введите ваше имя ");
+//string username = Console.ReadLine();
+//Console.Write("Привет, ");
+//Console.Write(username);
