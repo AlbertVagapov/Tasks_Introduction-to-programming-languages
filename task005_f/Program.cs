6e576538182293e325377969869bd112a431ee9e
Albert Vagapov <va3@list.ru>
2022-02-02 14:50:12 +0500

    t6 done completeli

---
+++ task005_f/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число a: ");
+
+int a = int.Parse(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
---
+++ task006_ifchet/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+if( a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
