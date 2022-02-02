131f78833c68522da2d09c19b8ff6724f6c3d21d
Albert Vagapov <va3@list.ru>
2022-02-02 11:10:17 +0500

    Add t2 more less

---
+++ task002_More_less/Program.cs
@@ -0,0 +1,20 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a>b)
+{
+    Console.WriteLine("a больше b");
+}
+else if(a==b)
+{
+    Console.WriteLine("a равно b");
+}
+else
+{
+    Console.WriteLine("a меньшне b");
+}
---
+++ task002_if_n^2/Program.cs
@@ -0,0 +1,16 @@
+﻿// По двум заданным числам a и b проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b * b)
+{
+    Console.WriteLine("a = b^2: ");
+} 
+else
+{
+    Console.WriteLine("a не равняется b^2: ");
+}
