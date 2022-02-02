c9640b10c1dacd2fb8e9153a350bd1f4d7a1a0db
Albert Vagapov <va3@list.ru>
2022-02-02 13:31:09 +0500

    task4 done

--- task004_find_max/Program.cs
+++ task004_find_max/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимальное из трех чисел
+
+int a = 99;
+int b = 88;
+int c = 88;
+
+if(a > b)
+{
+    Console.WriteLine("Nubmer Max = " + a); 
+}
+else if(b > c)
+{
+    Console.WriteLine("Number Max = " + b);
+}
+else
+{
+    Console.WriteLine("Number Max = " + c);
+}
