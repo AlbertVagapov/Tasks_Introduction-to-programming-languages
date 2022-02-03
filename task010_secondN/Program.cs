8221f8c04fa28168993dfc1819ba5b9913abc3e8
Albert Vagapov <va3@list.ru>
2022-02-03 20:08:25 +0500

    t10 done

---
+++ task010_secondN/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать вторую цифру трёхзначного числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a > 100 && a < 999)
+{
+    int b = a % 100 / 10;
+    Console.WriteLine("вторая цифра: " + b);
+}
+else
+{
+    Console.WriteLine("Вы не ввели трехзначное число");
+}
