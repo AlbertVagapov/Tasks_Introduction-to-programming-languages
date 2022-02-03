8580294c0480a091bbaf5b7a304475508f88cbe0
Albert Vagapov <va3@list.ru>
2022-02-03 21:02:05 +0500

    t14 done

---
+++ task014_3dN/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(n>99 ? n.ToString()[2] : "Введте число большее 99");
