7de84b4a2e199ec12e7a528b29f3305e89fd20a5
Albert Vagapov <va3@list.ru>
2022-02-04 10:59:55 +0500

    t16 done

--- task014_3dN/Program.cs
+++ task014_3dN/Program.cs
@@ -3,5 +3,4 @@
 Console.WriteLine("Введите число: ");
 int n = int.Parse(Console.ReadLine());
 
-
 Console.WriteLine(n>99 ? n.ToString()[2] : "Введте число большее 99");
---
+++ task016_weekend/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите номер дня недели");
+int a = int.Parse(Console.ReadLine());
+
+if (a >= 1 && a <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+else if (a >= 6 && a <= 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
