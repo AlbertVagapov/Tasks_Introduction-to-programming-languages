c390966c8739ae92e75d040e47243311c2f1658f
Albert Vagapov <va3@list.ru>
2022-02-09 22:49:07 +0500

    t17 done

---
+++ task017_a=b^2/Program.cs
@@ -0,0 +1,19 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+//int a = new Random().Next(1, 99);
+//int b = new Random().Next(1, 99);
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b * b | b == a * a)
+{
+    Console.WriteLine("число является квадратом другого");
+}
+else
+{
+    Console.WriteLine("число не является квадратом другого");
+}
