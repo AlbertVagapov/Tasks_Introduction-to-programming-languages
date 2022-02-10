734c444131c26ba46e8fd6e3a4165dd4f01921ae
Albert Vagapov <va3@list.ru>
2022-02-10 11:21:53 +0500

    t23 done

--- task023_N^2/Program.cs
+++ task023_N^2/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Показать таблицу квадратов чисел от 1 до N
 
-Console.WriteLine("Введите число a: ");
-int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
 
-int number
+for (int i = 0; i <= N; i++)
+{
+    Console.WriteLine($"{i}*{i}={i * i}");
+}
