d7d1519f9a2ae329b06094f4ead6e9e0deb31316
Albert Vagapov <va3@list.ru>
2022-02-10 09:35:18 +0500

    t24 done

---
+++ task024_n^3/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+int index = 1;
+
+while (index <= N)
+{ 
+    sum = index*index*index;
+    Console.WriteLine(sum);
+    index++;
+}
