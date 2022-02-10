71311c5a3bf6c2b21895b636ea534904761510bf
Albert Vagapov <va3@list.ru>
2022-02-10 09:00:27 +0500

    t20 done

---
+++ task020_quater/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти: ");
+int x = int.Parse(Console.ReadLine());
+
+if (x >= 1 && x <= 4)
+{
+    if (x == 1)
+    {
+        Console.WriteLine("x > 0, y > 0");
+    }
+    else if (x==2)
+    {
+        Console.WriteLine("x < 0, y > 0");
+    }
+    else if(x==3)
+    {
+        Console.WriteLine("x < 0, y < 0");
+    }
+    else if(x==4)
+    {
+        Console.WriteLine("x > 0, y < 0");
+    }
+}
+else
+{
+    Console.WriteLine("Введите целое число от 1 до 4");
+}
---
+++ task023_N^2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+int number
