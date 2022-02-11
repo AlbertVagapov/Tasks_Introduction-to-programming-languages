a27c1e2c4f189a4aadaf509bef38aaf944313988
Albert Vagapov <va3@list.ru>
2022-02-11 10:43:36 +0500

    t18 done

--- task018_iftruefalse/Program.cs
+++ task018_iftruefalse/Program.cs
@@ -2,29 +2,43 @@
 
 
 
-bool x = true;
-bool y = false;
+// bool x = true;
+// bool y = false;
 
-if(x)
-    Console.WriteLine("x");  // выполниется если условие true
-else
-    Console.WriteLine("не x");  // выполнится если предыдущие условия не сработали(если false)
-
-
-if(!x)  // отрицание
-    Console.WriteLine("!x");
-else
-    Console.WriteLine("не !x");
+// if(x)
+//     Console.WriteLine("x");  // выполниется если условие true
+// else
+//     Console.WriteLine("не x");  // выполнится если предыдущие условия не сработали(если false)
 
 
-if(x && y)  // логическое И
-    Console.WriteLine("x && y");
-else
-    Console.WriteLine("не x && y");
+// if(!x)  // отрицание
+//     Console.WriteLine("!x");
+// else
+//     Console.WriteLine("не !x");
 
 
-if(x || y)  //логическое ИЛИ
-    Console.WriteLine("x || y");
-else
-    Console.WriteLine("не x || y");
+// if(x && y)  // логическое И
+//     Console.WriteLine("x && y");
+// else
+//     Console.WriteLine("не x && y");
 
+
+// if(x || y)  //логическое ИЛИ
+//     Console.WriteLine("x || y");
+// else
+//     Console.WriteLine("не x || y");
+
+foreach (bool x in new[] { false, true })
+{
+    foreach (bool y in new[] { false, true })
+    {
+        if (!(x | y) == (!x & !y))
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+    }        
+}
---
+++ task022_2D_3D/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+Console.WriteLine("Введите значение x1: ");
+double x1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение y1: ");
+double y1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение x2: ");
+double x2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение y2: ");
+double y2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение z2: ");
+double z2 = double.Parse(Console.ReadLine());
+
+int L =  Convert.ToInt32(math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + z2 * z2));
+Console.WriteLine(L);
