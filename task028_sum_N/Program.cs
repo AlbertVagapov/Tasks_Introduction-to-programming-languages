bb9ad6abc52d0cfec1ba7922c7f98a5f37690a0c
Albert Vagapov <va3@list.ru>
2022-02-14 22:42:30 +0500

    t28

---
+++ task028_sum_N/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитайте сумму цифр в числе.
+//A.ToString().Length
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int s = 0;
+while (a > 0)
+{
+    s = s + a % 10;
+    a = a / 10;
+}
+Console.WriteLine("сумма цифр в числе - " + s);
