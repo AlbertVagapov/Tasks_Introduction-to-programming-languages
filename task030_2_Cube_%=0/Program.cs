b7c0e7e8005ac667c00f7fec0a979101c073cc30
Albert Vagapov <va3@list.ru>
2022-02-14 23:37:26 +0500

    t30 done

---
+++ task030_2_Cube_%=0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Покажите кубы чисел, заканчивающихся на чётную цифру.
+
+Console.WriteLine("Введите до какого числа");
+int number = int.Parse(Console.ReadLine());
+
+// int b = cube % 2;
+int index = 0;
+int sum = 0;
+int count = 1;
+
+Console.WriteLine();
+
+while (count <= number)
+{
+    sum = count * count * count;
+    if (sum % 2 == 0)
+    {
+        Console.WriteLine($"куб от {count} равен {sum} и заканчивается на четную цифру");        
+    }
+    count++;
+}
---
+++ task030_Cube_%=0/Program.cs
@@ -0,0 +1,16 @@
+﻿// Покажите кубы чисел, заканчивающихся на чётную цифру.
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+int cube = a * a * a;
+int b = cube % 2;
+
+if (b==0)
+{
+    Console.WriteLine($"куб {a} числа равен {cube} и заканчивается на четную цифру");
+}
+else
+{
+    Console.WriteLine($"куб {a} числа равен {cube} и заканчивается на нечетную цифру"); 
+}
