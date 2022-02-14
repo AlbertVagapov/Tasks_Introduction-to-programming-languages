6905ecdc47dba6fa8d33927aa9dc200c6c753d7d
Albert Vagapov <va3@list.ru>
2022-02-14 21:29:27 +0500

    t26 done

---
+++ task026_a^b/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень B");
+int B = int.Parse(Console.ReadLine());
+
+double sum = 0;
+int index = 0;
+Console.WriteLine();
+while (index <= B)
+{
+    sum = Math.Pow(A, index);    
+    Console.WriteLine($"{A}^{index} = {sum}");
+    index++;
+}
