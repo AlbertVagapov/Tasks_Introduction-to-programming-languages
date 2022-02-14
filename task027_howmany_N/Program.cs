f016df6576c7df3cdde433796138b06b38418d5f
Albert Vagapov <va3@list.ru>
2022-02-14 22:09:51 +0500

    t27 done

---
+++ task027_howmany_N/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+int B = (int)Math.Log10(A) + 1;
+
+Console.WriteLine("цифр в числе - "+B);
