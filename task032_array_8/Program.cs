d06f2480e7db8d1decdd3e9a203ee67d9a843711
Albert Vagapov <va3@list.ru>
2022-02-15 11:11:03 +0500

    t32 done

---
+++ task032_array_8/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+
+Console.WriteLine("Введите колличество элементов в массиве: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[a];
+
+FillArray(array);
+PrintArray(array);
