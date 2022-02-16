26c928e0b775eaaa7825d80b8263ee2308a50b4e
Albert Vagapov <va3@list.ru>
2022-02-16 10:56:38 +0500

    t33 done

---
+++ task033_array12/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. 
+// Найти сумму положительных/отрицательных элементов массива
+
+int[] array = new int [12];
+int sum_plus = 0,
+    sum_minus = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+
+    if (array[i] > 0)
+        sum_plus += array[i];
+    else
+        sum_minus += array[i];
+}
+
+Console.WriteLine(sum_plus);
+Console.WriteLine(sum_minus);
+
