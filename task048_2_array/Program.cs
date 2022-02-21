6b96da546e12ac4c88975d776a263f0069d9efb8
Albert Vagapov <va3@list.ru>
2022-02-21 13:57:14 +0500

    Seminar lesson 21 02 2022

---
+++ task048_2_array/Program.cs
@@ -0,0 +1,33 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+Console.WriteLine("Введите число");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 100) ; // заполняем числами [0,100)
+        }
+    }
+}
+int[,] array = new int[m,n];
+FillArray(array);
+PrintArray(array);
+
+
---
+++ task050_array_-1/Program.cs
@@ -0,0 +1,41 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 100); // заполняем числами [0,100)
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] % 2 == 0) array[i, j] = -array[i, j];
+    }
+}
+Console.WriteLine();
+PrintArray(array);
