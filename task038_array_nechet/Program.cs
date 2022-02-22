e8d01079455f8f90dd7f7a57d8f6f5e8ab884b65
Albert Vagapov <va3@list.ru>
2022-02-22 23:21:59 +0500

    t38 done

--- task038_array_nechet/Program.cs
+++ task038_array_nechet/Program.cs
@@ -1,3 +1,17 @@
 ﻿// Найти сумму чисел одномерного массива, стоящих на нечётной позиции.
 
-Console.WriteLine("Hello, World!");
+int[] array = new int[6];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    Console.Write(array[i]);
+    Console.Write(" ");
+    if (i % 2 == 1)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine(sum);
