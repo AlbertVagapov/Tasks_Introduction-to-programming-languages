a42179bfe78ab8bb2b68863bf5be18f69cb0cb70
Albert Vagapov <va3@list.ru>
2022-02-23 11:18:30 +0500

    t49 done

---
+++ task049_m_n/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать двумерный массив размером m×n, заполненный вещественными числами
+
+Console.WriteLine("Введите число");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F2}", matr[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Random rand = new Random();
+
+            matr[i, j] = rand.NextDouble()*100;
+        }
+    }
+}
+double[,] array = new double[m,n];
+FillArray(array);
+PrintArray(array);
