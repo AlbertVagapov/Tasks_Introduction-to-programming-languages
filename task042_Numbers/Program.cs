718bb572aff123f2b4be4807d2ffed1a1771aad8
Albert Vagapov <va3@list.ru>
2022-02-23 08:59:16 +0500

    t42 done

---
+++ task042_Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿//Определить сколько чисел больше 0 введено с клавиатуры
+
+int count = 0;
+while(true)
+{
+    Console.WriteLine("Введите целое число, для выхода введите не число: ");
+    string input = Console.ReadLine();
+
+    bool isNum = int.TryParse(input, out int chNum);
+    if (isNum && chNum > 0) count++;
+    else if (isNum && chNum <= 0) continue;
+    else break;
+}
+Console.WriteLine($"Вы ввели {count} чисел больше 0");
+
