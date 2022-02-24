35e2efc6cba46e5e3e718ba7cc00fc9a5c07e444
Albert Vagapov <va3@list.ru>
2022-02-24 14:32:31 +0500

    seminar lessons 24.02.2022

---
+++ task058/Program.cs
@@ -0,0 +1,49 @@
+﻿//58. Написать программу, 
+//которая в двумерном массиве заменяет строки
+// на столбцы или сообщить, что это невозможно (в случае, если матрица не квадратная).
+void FillArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 6;
+int n = 6;
+int[,] array = new int[m, n];
+FillArr(array);
+PrintArr(array);
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        Console.Write(array[j, i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int[,] mass = new int[array.GetLength(0), array.GetLength(1)];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = array[j, i];
+    }
+}
+PrintArr(mass);
---
+++ task062_cross_array/Program.cs
@@ -0,0 +1,53 @@
+﻿// В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+
+void FillArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 3;
+int n = 3;
+int[,] array = new int[m, n];
+FillArr(array);
+PrintArr(array);
+Console.WriteLine();
+
+int x = 0;
+int y = 0;
+
+int min = array[0, 0];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] < min)
+        {
+            min = array[i, j];
+            x = i;
+            y = j;
+        }
+    }
+}
+int[,] mas = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+
+for ()
+
+
+
+    Console.Write(min);
